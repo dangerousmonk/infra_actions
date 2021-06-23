846017f8f5d75442c10ed99f632faa93ab8809eb
Avilov Oleg <dangerousmonk@yandex.ru>
2021-06-23 20:11:05 +0300

    Add exceptions to workflow

--- infra_project/infra_project/urls.py
+++ infra_project/infra_project/urls.py
@@ -1,6 +1,3 @@
-from django.contrib import admin
-from django.urls import path, include
-
 """infra_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -16,6 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
+from django.urls import path, include
+
 urlpatterns = [
     path('', include('infra_app.urls', namespace='infra_app')),
     path('admin/', admin.site.urls),
